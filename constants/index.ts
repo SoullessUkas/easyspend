1663aa8be36aa6f9abbd8344dddf7eeff17f2243
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-10-02 21:01:37 -0400

    Implement sidebar desktop and MobaleNavegation

--- constants/index.ts
+++ constants/index.ts
@@ -2,22 +2,22 @@ export const sidebarLinks = [
   {
     imgURL: "/icons/home.svg",
     route: "/",
-    label: "Home",
+    label: "Principal",
   },
   {
     imgURL: "/icons/dollar-circle.svg",
     route: "/my-banks",
-    label: "My Banks",
+    label: "Meus bancos",
   },
   {
     imgURL: "/icons/transaction.svg",
     route: "/transaction-history",
-    label: "Transaction History",
+    label: "Historico de transações",
   },
   {
     imgURL: "/icons/money-send.svg",
     route: "/payment-transfer",
-    label: "Transfer Funds",
+    label: "Transferencia",
   },
 ];
 
