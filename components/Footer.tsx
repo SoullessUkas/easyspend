0552fab10e4a2cf817c692d0c25dea699c30fd80
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-11-02 14:07:35 -0400

    Finalize auth and implement sentry

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
   const handleLogOut = async () => {
     const loggedOut = await logoutAccount();
 
-    if(loggedOut) router.push('/sign-in')
+   if(loggedOut) router.push('/sign-in')
   }
   return (
     <footer className="footer">
